725af0713b03a697207b11d2de0c8b3c85ad068f
Kevin Childs <kchilds2020@gmail.com>
2023-03-14 11:30:50 -0500

    added default keyword to switch

--- src/main/java/com/revature/console/ConsoleMenu.java
+++ src/main/java/com/revature/console/ConsoleMenu.java
@@ -26,24 +26,31 @@ public class ConsoleMenu implements Menu {
             int option = input.retrieveNum("Option: ");
 
             switch(option){
+                //create todo case
                 case 1: 
                     String taskName = input.retrieveString("Task Name: ");
                     Todo newTodo = todoService.createTodo(new Todo(taskName));
                     System.out.println("New Todo Created: " + newTodo);
                     break;
+                //mark complete case
                 case 2: 
                     int updateTodoId = input.retrieveNum("Todo Id To Update: ");
                     Todo updatedTodo = todoService.updateTodo(updateTodoId);
                     if(updatedTodo != null) System.out.println("Todo with id " + updateTodoId + " marked as complete");
                     else System.out.println("Todo with id " + updateTodoId + " does not exist in system");
                     break;
+                //delte todo case
                 case 3: 
                     int deletedTodoId = input.retrieveNum("Todo Id To Delete: ");
                     todoService.deleteTodo(deletedTodoId);
                     break;
+                //exit app
                 case 0:
                     running = false; 
                     break;
+                //invalid input
+                default:
+                    System.out.println("invalid input...");
             }
 
         }
